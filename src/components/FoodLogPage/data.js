9537dae7442c7b6f1975163611fd2eb1fde73e4a
Joe Maurer <jmaurer1994@gmail.com>
2022-04-25 05:00:53 +0000

    add favicon and title

--- src/components/FoodLogPage/data.js
+++ src/components/FoodLogPage/data.js
@@ -3,7 +3,7 @@ export const meals = [
         name: 'Breakfast',
         date: '4/21/2022',
         description:
-            'Whisk 4 eggs in a bowl, mix in a pinch of shredded cheese. Sautee 2tbsp minced garlic in a pan for 30 seconds on low-med heat. Once garlic begins to brown pour eggs into pan. After 3-4 minutes, fold egg over and begin to scramble. Add 1 cup chopped spinach to empty side of pan and cover for 3-4 min, stir as needed. Once spinach is tender, mix into egg scramble. Garnish with shredded cheese.',
+            'Whisk 4 eggs in a bowl, mix in a pinch of shredded cheese. Sautee 2tbsp minced garlic in a pan for 30 seconds on low-med heat. Once garlic begins to brown pour eggs into pan. After 3-4 minutes, fold egg over and begin to scramble. Add 1 cup chopped spinach to empty side of pan and cover for 3-4 min, stir as needed. Once spinach is tender, mix into egg scramble. Garnish with shredded cheese, salt and pepper to taste.',
     },
     {
         name: 'Lunch',
